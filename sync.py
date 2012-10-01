30d8c544e9f6667fa29300d43b1370e0cad13db5
Eric Mill <konklone@gmail.com>
2012-10-01 15:36:19 -0400

    It works

--- sync.py
+++ sync.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, os, glob, errno
+import sys, os, glob, errno, subprocess
 import yaml
 import zipfile, zlib
 from github import Github
@@ -13,32 +13,35 @@ def main():
   config = yaml.load(open("config.yml", 'r'))
   options = get_options()
 
-  output_dir = os.path.join(os.getcwd(), "output/bills") # absolute path
-  mkdir_p(output_dir)
-
   session = options.get('session', None)
   if not session:
     print "Provide a --session option."
     return
 
+  output_dir = os.path.join(os.getcwd(), "output/bills") # absolute path
+  mkdir_p(output_dir)
 
-  print "Locating all data.json files on disk..."
+  zipfile_name = "bills-%s.zip" % session
+  zipfile_path = os.path.join(output_dir, zipfile_name)
 
-  data_dir = config['data_directory']
+  if os.path.exists(zipfile_path) and options.get('cache', False):
+    print "Using cached zip file..."
 
-  # switch working directory to input data dir for relative globs
-  os.chdir(os.path.join(data_dir, "bills", session))
-  files = glob.glob("*/*/data.json")
+  else:
+    print "Zipping up data.json files..."
+
+    data_dir = config['data_directory']
+
+    # switch working directory to input data dir for relative globs
+    os.chdir(os.path.join(data_dir, "bills", session))
+    files = glob.glob("*/*/data.json")
+
+    with zipfile.ZipFile(zipfile_path, "w", zipfile.ZIP_DEFLATED) as zf:
+      for f in files:
+        zf.write(f)
 
 
-  print "Creating zip file..."
-
-  with zipfile.ZipFile(os.path.join(output_dir, "bills-%s.zip" % session), "w", zipfile.ZIP_DEFLATED) as zf:
-    for f in files:
-      zf.write(f)
-
-  
-  print "Connecting to Github..."
+  print "Creating download on Github..."
 
   username = config['github']['credentials']['username']
   password = config['github']['credentials']['password']
@@ -47,6 +50,35 @@ def main():
   organization = client.get_organization(config['github']['organization'])
   repo = organization.get_repo(config['github']['repo'])
 
+  for download in repo.get_downloads():
+    if download.name == zipfile_name:
+      print "Deleting existing download..."
+      download.delete()
+
+  download = repo.create_download(
+    zipfile_name, os.path.getsize(zipfile_path), 
+    "Bill data for Congress #%s" % session, "application/zip")
+
+  
+  print "Uploading file to S3..."
+
+  os.chdir(output_dir)
+
+  subprocess.call(["curl",
+    '-F key=%s' % download.path,
+    '-F acl=%s' % download.acl,
+    '-F success_action_status=201',
+    '-F Filename=%s' % download.name,
+    '-F AWSAccessKeyId=%s' % download.accesskeyid,
+    '-F Policy=%s' % download.policy,
+    '-F Signature=%s' % download.signature,
+    '-F Content-Type=%s' % download.mime_type,
+    '-F file=@%s' % zipfile_name,
+    download.s3_url
+  ], stdout=open("/dev/null"))
+
+  print "Uploaded %s to Github." % zipfile_name
+  exit(0)
 
 
 def get_options():
